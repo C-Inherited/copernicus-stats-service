ee0f62b3158cac7554deaa51108d2bdabbe8624c
Celia <celiapdg@hotmail.com>
2021-02-21 20:17:08 +0100

    Updated controllers' routes

--- src/main/java/com/copernicus/stats/controller/impl/AccountStatsController.java
+++ src/main/java/com/copernicus/stats/controller/impl/AccountStatsController.java
@@ -13,25 +13,25 @@ public class AccountStatsController {
     @Autowired
     private AccountStatsService accountStatsService;
 
-    @GetMapping("/stats/findMeanEmployeeCount")
+    @GetMapping("/account/avg/employee-count")
     @ResponseStatus(HttpStatus.OK)
     public Double avgEmployeeCount(){
         return accountStatsService.avgEmployeeCount();
     }
 
-    @GetMapping("/stats/orderEmployeeCount")
+    @GetMapping("/account/median/employee-count")
     @ResponseStatus(HttpStatus.OK)
     public Double medianEmployeeCount(){
         return accountStatsService.medianEmployeeCount();
     }
 
-    @GetMapping("/stats/findMinEmployeeCount")
+    @GetMapping("/account/min/employee-count")
     @ResponseStatus(HttpStatus.OK)
     public Integer minEmployeeCount(){
         return accountStatsService.minEmployeeCount();
     }
 
-    @GetMapping("/stats/findMaxEmployeeCount")
+    @GetMapping("/account/max/employee-count")
     @ResponseStatus(HttpStatus.OK)
     public Integer maxEmployeeCount(){
         return  accountStatsService.maxEmployeeCount();
--- src/main/java/com/copernicus/stats/controller/impl/OpportunityStatsController.java
+++ src/main/java/com/copernicus/stats/controller/impl/OpportunityStatsController.java
@@ -14,62 +14,62 @@ public class OpportunityStatsController implements IOpportunityStatsController {
     @Autowired
     IOpportunityService opportunityService;
 
-    @GetMapping("/opportunities/count/by/product")
+    @GetMapping("/opportunity/count/by/product")
     public List<Object[]> countOpportunitiesByProduct(Optional<String> status) {
         return opportunityService.countOpportunitiesByProduct(status);
     }
 
-    @GetMapping("/opportunities/count/by/city")
+    @GetMapping("/opportunity/count/by/city")
     public List<Object[]> countOpportunitiesByCity(Optional<String> status) {
         return opportunityService.countOpportunitiesByCity(status);
     }
 
-    @GetMapping("/opportunities/count/by/country")
+    @GetMapping("/opportunity/count/by/country")
     public List<Object[]> countOpportunitiesByCountry(Optional<String> status) {
         return opportunityService.countOpportunitiesByCountry(status);
     }
 
-    @GetMapping("/opportunities/count/by/industry")
+    @GetMapping("/opportunity/count/by/industry")
     public List<Object[]> countOpportunitiesByIndustry(Optional<String> status) {
         return opportunityService.countOpportunitiesByIndustry(status);
     }
 
-    @GetMapping("/opportunities/avg/by/account")
+    @GetMapping("/opportunity/avg/by/account")
     public Double avgOpportunitiesByAccount() {
         return opportunityService.avgOpportunitiesByAccount();
     }
 
-    @GetMapping("/opportunities/avg/quantity/by/product")
+    @GetMapping("/opportunity/avg/quantity/by/product")
     public List<Object[]> avgQuantityByProduct() {
         return opportunityService.avgQuantityByProduct();
     }
 
-    @GetMapping("/opportunities/max/by/account")
+    @GetMapping("/opportunity/max/by/account")
     public Integer maxOpportunitiesByAccount() {
         return opportunityService.maxOpportunitiesByAccount();
     }
 
-    @GetMapping("/opportunities/max/quantity/by/product")
+    @GetMapping("/opportunity/max/quantity/by/product")
     public List<Object[]> maxQuantityByProduct() {
         return opportunityService.maxQuantityByProduct();
     }
 
-    @GetMapping("/opportunities/min/by/account")
+    @GetMapping("/opportunity/min/by/account")
     public Integer minOpportunitiesByAccount() {
         return opportunityService.minOpportunitiesByAccount();
     }
 
-    @GetMapping("/opportunities/min/quantity/by/product")
+    @GetMapping("/opportunity/min/quantity/by/product")
     public List<Object[]> minQuantityByProduct() {
         return opportunityService.minQuantityByProduct();
     }
 
-    @GetMapping("/opportunities/median/by/account")
+    @GetMapping("/opportunity/median/by/account")
     public Double medianOpportunitiesByAccount() {
         return opportunityService.medianOpportunitiesByAccount();
     }
 
-    @GetMapping("/opportunities/median/quantity/by/product")
+    @GetMapping("/opportunity/median/quantity/by/product")
     public List<Object[]> medianQuantityByProduct() {
         return opportunityService.medianQuantityByProduct();
     }
